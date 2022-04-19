4f595b4f6f26225aec5dd4532ad0716c4aaaf295
Philip <pk@click-solutions.de>
2022-04-19 15:41:55 +0200

    feat: mockdata, types

---
+++ src/mockdata.tsx
@@ -0,0 +1,28 @@
+const mockSubjects = [
+  {
+    id: '1',
+    userId: '1',
+    name: 'math',
+    colorCode: '#234567',
+  },
+  {
+    id: '2',
+    userId: '1',
+    name: 'english',
+    colorCode: '#726930',
+  },
+  {
+    id: '3',
+    userId: '1',
+    name: 'info',
+    colorCode: '#987654',
+  },
+  {
+    id: '4',
+    userId: '1',
+    name: 'physics',
+    colorCode: '#122397',
+  },
+];
+
+export { mockSubjects };
