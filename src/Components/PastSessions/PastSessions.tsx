e6875ec19a567d620678bbb427b843f1a45ec08f
Philip <pk@click-solutions.de>
2022-06-18 11:01:41 +0200

    feat: placeholder for past sessions

--- src/Components/PastSessions/PastSessions.tsx
+++ src/Components/PastSessions/PastSessions.tsx
@@ -9,6 +9,7 @@ import './PastSessions.scss';
 type Props = {
   content: {
     headline: string;
+    placeholder: string;
     cards: {
       datePrefix: string;
       retentionSuffix: string;
@@ -19,12 +20,16 @@ type Props = {
 };
 
 const PastSessions = ({ content, sessions }: Props) => {
+  const isPlaceholderVisible = sessions.length < 1;
   const sessionCards = sessions.map((session) => {
     return <SessionCard key={session.id} session={session} content={content.cards} />;
   });
   return (
     <div className='past-sessions'>
-      <div className='past-sessions__sessions-container'>{sessionCards}</div>
+      {isPlaceholderVisible && <p className='past-sessions__placeholder'>{content.placeholder}</p>}
+      {!isPlaceholderVisible && (
+        <div className='past-sessions__sessions-container'>{sessionCards}</div>
+      )}
     </div>
   );
 };
